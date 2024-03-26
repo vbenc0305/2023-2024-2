da881c9af690a9049a7b4c5bd51424d9f2d25de9
vbenc <vargabence0305@gmail.com>
2024-03-26 14:28:33 +0100

    init

--- biro.py
+++ biro.py
@@ -2,3 +2,8 @@ import tkinter as tk
 
 def main():
     win=tk.TK()
+    canvas = tk.Canvas(win,width=700, height=500)
+    canvas.grid(columspan=2, rowspan=5)
+
+if __name__ == '__main__':
+    main()
